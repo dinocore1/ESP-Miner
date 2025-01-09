6edb1d365e750decb1e7d8fa7541ea212003a45b
Paul Soucy <paul@nlighten-systems.com>
2025-01-08 20:59:42 -0500

    wip

--- main/STUSB4500.c
+++ main/STUSB4500.c
@@ -16,19 +16,42 @@ static i2c_master_dev_handle_t stusb4500_dev_handle;
 
 static const char * TAG = "STUSB4500";
 
-static SemaphoreHandle_t alertSemaphore = NULL;
+static TaskHandle_t taskHandle = NULL;
+
+// The index within the target task's array of task notifications to use.
+static const UBaseType_t xArrayIndex = 0;
+
+static void stusb4500_task(void* params);
 
 static void alert_isr_handler(void *arg)
 {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-    xSemaphoreGiveFromISR(alertSemaphore, &xHigherPriorityTaskWoken);
+    vTaskNotifyGiveFromISR( taskHandle, &xHigherPriorityTaskWoken );
     portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
 }
 
 static void set_PDOSnk_count(uint8_t const count)
 {
     i2c_bitaxe_register_write_byte(stusb4500_dev_handle, REG_DPM_PDO_NUM, count);
+}
+
+static void stusb4500_task(void* params)
+{
+    const TickType_t xBlockTime = pdMS_TO_TICKS( 500 );
+    uint32_t ulNotifiedValue;
+
+    for(;;) {
+
+        ulNotifiedValue = ulTaskNotifyTakeIndexed( xArrayIndex,
+                                                   pdTRUE,
+                                                   xBlockTime );
+
+        while (ulNotifiedValue > 0) {
+            ulNotifiedValue--;
+        }
+
+    }
 }
 
 esp_err_t STUSB4500_init()
@@ -48,7 +71,7 @@ esp_err_t STUSB4500_init()
 
     ESP_RETURN_ON_ERROR(gpio_isr_handler_add(ALERT_PIN, alert_isr_handler, NULL), TAG, "adding ISR handler");
 
-    alertSemaphore = xSemaphoreCreateCounting(10, 0);
+    xTaskCreate(stusb4500_task, TAG, 4096, NULL, 10, taskHandle);
 
     set_PDOSnk_count(1);
 
@@ -57,10 +80,6 @@ esp_err_t STUSB4500_init()
         uint8_t read_buf[1];
         i2c_bitaxe_register_read(stusb4500_dev_handle, reg_addr, read_buf, 1);
     }
-
-
-
-
 
     return ESP_OK;
 }
