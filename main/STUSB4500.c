cbaa2e771ad21043fbbf75d826f85f3218822148
Paul Soucy <paulsoucy1@gmail.com>
2025-01-08 22:40:47 -0500

    wip

--- main/STUSB4500.c
+++ main/STUSB4500.c
@@ -1,6 +1,6 @@
-#include "esp_log.h"
-#include <esp_check.h>
 #include <driver/gpio.h>
+#include <esp_check.h>
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
@@ -12,6 +12,12 @@
 #define STUSB4500_I2CADDR_DEFAULT 0x28
 #define ALERT_PIN 22
 
+#define LE16(addr) (((uint16_t) (*((uint8_t *) (addr)))) + (((uint16_t) (*(((uint8_t *) (addr)) + 1))) << 8))
+
+#define LE32(addr)                                                                                                                 \
+    ((((uint32_t) (*(((uint8_t *) (addr)) + 0))) + (((uint32_t) (*(((uint8_t *) (addr)) + 1))) << 8) +                             \
+      (((uint32_t) (*(((uint8_t *) (addr)) + 2))) << 16) + (((uint32_t) (*(((uint8_t *) (addr)) + 3))) << 24)))
+
 static i2c_master_dev_handle_t stusb4500_dev_handle;
 
 static const char * TAG = "STUSB4500";
@@ -21,14 +27,19 @@ static TaskHandle_t taskHandle = NULL;
 // The index within the target task's array of task notifications to use.
 static const UBaseType_t xArrayIndex = 0;
 
-static void stusb4500_task(void* params);
+static STUSB_GEN1S_ALERT_STATUS_RegTypeDef status;
+static STUSB_GEN1S_PRT_STATUS_RegTypeDef PRT_status;
+static uint8_t num_src_pdo;
+static USB_PD_SRC_PDOTypeDef src_pdo[7];
 
-static void alert_isr_handler(void *arg)
+static void stusb4500_task(void * params);
+
+static void alert_isr_handler(void * arg)
 {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-    vTaskNotifyGiveFromISR( taskHandle, &xHigherPriorityTaskWoken );
-    portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+    vTaskNotifyGiveFromISR(taskHandle, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 static void set_PDOSnk_count(uint8_t const count)
@@ -36,21 +47,59 @@ static void set_PDOSnk_count(uint8_t const count)
     i2c_bitaxe_register_write_byte(stusb4500_dev_handle, REG_DPM_PDO_NUM, count);
 }
 
-static void stusb4500_task(void* params)
+static void stusb4500_task(void * params)
 {
-    const TickType_t xBlockTime = pdMS_TO_TICKS( 500 );
+    const TickType_t xBlockTime = pdMS_TO_TICKS(500);
     uint32_t ulNotifiedValue;
+    esp_err_t ret;
 
-    for(;;) {
+    uint8_t scratch[40];
 
-        ulNotifiedValue = ulTaskNotifyTakeIndexed( xArrayIndex,
-                                                   pdTRUE,
-                                                   xBlockTime );
+    for (;;) {
+
+        ulNotifiedValue = ulTaskNotifyTakeIndexed(xArrayIndex, pdTRUE, xBlockTime);
 
         while (ulNotifiedValue > 0) {
             ulNotifiedValue--;
-        }
 
+            ESP_GOTO_ON_ERROR(i2c_bitaxe_register_read(stusb4500_dev_handle, REG_ALERT_STATUS_1, scratch, 2), err, TAG,
+                              "reading status reg");
+            status.d8 = scratch[0] & ~scratch[1];
+
+            if (status.b.PRT_STATUS_AL) {
+
+                ESP_GOTO_ON_ERROR(i2c_bitaxe_register_read(stusb4500_dev_handle, REG_PRT_STATUS, &PRT_status.d8, 1), err, TAG,
+                                  "reading PRT_status reg");
+
+                if (PRT_status.b.MSG_RECEIVED) {
+                    USBPD_MsgHeader_TypeDef header;
+
+                    ESP_GOTO_ON_ERROR(i2c_bitaxe_register_read(stusb4500_dev_handle, REG_RX_HEADER, scratch, 2), err, TAG,
+                                      "reading RX_HEADER");
+                    header.d16 = LE16(&scratch[0]);
+
+                    if (header.b.NumberOfDataObjects > 0) {
+                        switch (header.b.MessageType) {
+                        case 0x01:
+                            ESP_GOTO_ON_ERROR(i2c_bitaxe_register_read(stusb4500_dev_handle, REG_RX_DATA_OBJ, scratch,
+                                                                       header.b.NumberOfDataObjects * 4),
+                                              err, TAG, "read RX_DATA_OBJ");
+
+                            for (int i = 0; i < header.b.NumberOfDataObjects; i++) {
+                                src_pdo[i].d32 = LE32(&scratch[i * 4]);
+                            }
+                            num_src_pdo = header.b.NumberOfDataObjects;
+
+                            break;
+                        default:
+                            break;
+                        }
+                    }
+                }
+            }
+
+        err:
+        }
     }
 }
 
@@ -71,7 +120,7 @@ esp_err_t STUSB4500_init()
 
     ESP_RETURN_ON_ERROR(gpio_isr_handler_add(ALERT_PIN, alert_isr_handler, NULL), TAG, "adding ISR handler");
 
-    xTaskCreate(stusb4500_task, TAG, 4096, NULL, 10, taskHandle);
+    xTaskCreate(stusb4500_task, TAG, 4096, NULL, 10, &taskHandle);
 
     set_PDOSnk_count(1);
 
--- main/STUSB4500_def.h
+++ main/STUSB4500_def.h
@@ -1,43 +1,142 @@
 #ifndef STUSB4500_DEF_H_
 #define STUSB4500_DEF_H_
 
+#define REG_ALERT_STATUS_1 0x0B
+#define REG_PRT_STATUS 0x16
 #define REG_DPM_PDO_NUM 0x70
-
+#define REG_RX_HEADER 0x31
+#define REG_RX_DATA_OBJ 0x33
 
 typedef union
 {
-  uint32_t d32;
-  struct
-  {
-    //Table 6-9 Fixed Supply PDO - Source
-    uint32_t Max_Operating_Current : 10; // bits 9..0
-    uint32_t Voltage               : 10; // bits 19..10
-    uint8_t  PeakCurrent           :  2; // bits 21..20
-    uint8_t  Reserved              :  3; //
-    uint8_t  DataRoleSwap          :  1; // bits 25
-    uint8_t  Communication         :  1; // bits 26
-    uint8_t  ExternalyPowered      :  1; // bits 27
-    uint8_t  SuspendSuported       :  1; // bits 28
-    uint8_t  DualRolePower         :  1; // bits 29
-    uint8_t  FixedSupply           :  2; // bits 31..30
-  } fix;
-  struct
-  {
-    //Table 6-11 Variable Supply (non-Battery) PDO - Source
-    uint32_t Operating_Current : 10;
-    uint32_t Min_Voltage       : 10;
-    uint32_t Max_Voltage       : 10;
-    uint8_t  VariableSupply    :  2;
-  } var;
-  struct
-  {
-    //Table 6-12 Battery Supply PDO - Source
-    uint32_t Operating_Power : 10;
-    uint32_t Min_Voltage     : 10;
-    uint32_t Max_Voltage     : 10;
-    uint8_t  Battery         :  2;
-  } bat;
+    uint8_t d8;
+    struct
+    {
+        uint8_t PHY_STATUS_AL : 1;
+        uint8_t PRT_STATUS_AL : 1;
+        uint8_t _Reserved_2 : 1;
+        uint8_t PD_TYPEC_STATUS_AL : 1;
+        uint8_t HW_FAULT_STATUS_AL : 1;
+        uint8_t MONITORING_STATUS_AL : 1;
+        uint8_t CC_DETECTION_STATUS_AL : 1;
+        uint8_t HARD_RESET_AL : 1;
+    } b;
+} STUSB_GEN1S_ALERT_STATUS_RegTypeDef;
 
-} USB_PD_SRC_PDO_TypeDef;
+typedef union
+{
+    uint8_t d8;
+    struct
+    {
+        uint8_t HWRESET_RECEIVED : 1;
+        uint8_t HWRESET_DONE : 1;
+        uint8_t MSG_RECEIVED : 1;
+        uint8_t MSG_SENT : 1;
+        uint8_t BIST_RECEIVED : 1;
+        uint8_t BIST_SENT : 1;
+        uint8_t Reserved_6 : 1;
+        uint8_t TX_ERROR : 1;
+    } b;
+} STUSB_GEN1S_PRT_STATUS_RegTypeDef;
+
+typedef union
+{
+    uint8_t d8;
+    struct
+    {
+        uint8_t PHY_STATUS_AL_MASK : 1;
+        uint8_t PRT_STATUS_AL_MASK : 1;
+        uint8_t _Reserved_2 : 1;
+        uint8_t PD_TYPEC_STATUS_AL_MASK : 1;
+        uint8_t HW_FAULT_STATUS_AL_MASK : 1;
+        uint8_t MONITORING_STATUS_AL_MASK : 1;
+        uint8_t CC_DETECTION_STATUS_AL_MASK : 1;
+        uint8_t HARD_RESET_AL_MASK : 1;
+    } b;
+} STUSB_GEN1S_ALERT_STATUS_MASK_RegTypeDef;
+
+typedef union
+{
+    uint32_t d32;
+    struct
+    {
+        uint32_t Max_Operating_Current : 10;
+        uint32_t Voltage : 10;
+        uint8_t PeakCurrent : 2;
+        uint8_t Reserved : 2;
+        uint8_t Unchuncked_Extended : 1;
+        uint8_t Dual_RoleData : 1;
+        uint8_t Communication : 1;
+        uint8_t UnconstraintPower : 1;
+        uint8_t SuspendSupported : 1;
+        uint8_t DualRolePower : 1;
+        uint8_t FixedSupply : 2;
+    } fix;
+    struct
+    {
+        uint32_t Operating_Current : 10;
+        uint32_t Min_Voltage : 10;
+        uint32_t Max_Voltage : 10;
+        uint8_t VariableSupply : 2;
+    } var;
+    struct
+    {
+        uint32_t Operating_Power : 10;
+        uint32_t Min_Voltage : 10;
+        uint32_t Max_Voltage : 10;
+        uint8_t Battery : 2;
+    } bat;
+    struct
+    {
+        /*
+        uint8_t Max_Current :7;
+        uint8_t  Reserved0 :1;
+        uint8_t Min_Voltage:8;
+        uint8_t  Reserved1 :1;
+        uint8_t Max_Voltage:9;
+        uint8_t  Reserved2 :2;
+        uint8_t ProgDev : 2 ;
+        uint8_t Battery:2;
+        */
+        uint8_t Max_Current : 7;
+        uint8_t Reserved0 : 1;
+        uint16_t Min_Voltage : 8; /* to prevent packing issue ?? */
+        uint8_t Reserved1 : 1;
+        uint16_t Max_Voltage : 8;
+        uint8_t Reserved2 : 3;
+        uint8_t ProgDev : 2;
+        uint8_t Battery : 2;
+
+    } apdo;
+} USB_PD_SRC_PDOTypeDef;
+
+typedef union
+{
+    uint16_t d16;
+    struct
+    {
+#if defined(USBPD_REV30_SUPPORT)
+        uint16_t MessageType : /*!< Message Header's message Type                      */
+                               5;
+#else                           /* USBPD_REV30_SUPPORT */
+        uint16_t MessageType : /*!< Message Header's message Type                      */
+                               4;
+        uint16_t Reserved4 : /*!< Reserved                                           */
+                             1;
+#endif                          /* USBPD_REV30_SUPPORT */
+        uint16_t PortDataRole : /*!< Message Header's Port Data Role                    */
+                                1;
+        uint16_t SpecificationRevision : /*!< Message Header's Spec Revision                     */
+                                         2;
+        uint16_t PortPowerRole_CablePlug : /*!< Message Header's Port Power Role/Cable Plug field  */
+                                           1;
+        uint16_t MessageID : /*!< Message Header's message ID                        */
+                             3;
+        uint16_t NumberOfDataObjects : /*!< Message Header's Number of data object             */
+                                       3;
+        uint16_t Extended : /*!< Reserved                                           */
+                            1;
+    } b;
+} USBPD_MsgHeader_TypeDef;
 
 #endif // STUSB4500_DEF_H_
